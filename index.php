b81334c04714642f0ba3bae86acf44eaa129a90c
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2025-02-28 09:37:06 +0700

    Revert "Built in php web server routing"

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php
-require 'includes/cli-server-routing.php';
-include 'design/home/start.php';
-include 'design/home/head.php';
-include 'design/partials/header.php';
-include 'design/home/main.php';
-include 'design/partials/footer.php';
+<?php include 'design/home/start.php'; ?>
+<?php include 'design/home/head.php'; ?>
+<?php include 'design/partials/header.php'; ?>
+<?php include 'design/home/main.php'; ?>
+<?php include 'design/partials/footer.php'; ?>    
